4e9aa262bcae268c6fd0e26c438fa653f02bc690
Tom Mohr <tom-mohr@gmx.de>
2019-08-04 15:07:36 +0200

    - fixed bug (bottom sheet was hidden behind keyboard) - removed class "AndroidUtility" and used a better / simpler way to change status bar color

--- app/src/main/java/gt/com/gtnote/EditNoteActivity.java
+++ app/src/main/java/gt/com/gtnote/EditNoteActivity.java
@@ -3,6 +3,7 @@ package gt.com.gtnote;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -79,16 +81,16 @@ public class EditNoteActivity extends AppCompatActivity {
     private float requestedFontSize = currentFontSize;
     private int minRequestableFontSize = 8;
     private int maxRequestableFontSize = 64;
-    
-    // utility
-    private AndroidUtility utils = new AndroidUtility();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
-        
-        utils.makeTransparentStatusBar(this);
+    
+        // make it possible to change the status bar color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        }
     
         cssStyleSource = getString(R.string.note_webview_css);
         syntaxHighlightingJavascriptSource = readRawTextFile(R.raw.prism_js);
@@ -583,6 +585,10 @@ public class EditNoteActivity extends AppCompatActivity {
         // force android to render changes
         noteHeaderViewMode.invalidate();
         noteHeaderEditMode.invalidate();
+    
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(androidColor);
+        }
     }
 
     /**
